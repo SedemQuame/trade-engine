086349030ce35c79ab0ee4b8323e221cc6bff0e1
SedemQuame <sedem.anekpewu.3@gmail.com>
2020-11-21 18:54:48 +0000

    make it better

---
+++ src/test/java/com/trade/engine/TradeEngineTests.java
@@ -0,0 +1,13 @@
+package com.trade.engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeEngineTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
